8da75bbabc781c79f6f6f2f599acc44b02435ad0
Ross Barnowski <rossbar@berkeley.edu>
2023-06-15 09:39:22 -0700

    Blackify.

--- make_config.py
+++ make_config.py
@@ -2,13 +2,14 @@ from thefuzz import process
 import yaml
 import constants
 
-if __name__ == '__main__':
-    with open('./configs/meta_hickey.yaml', 'r', encoding="utf8") as stream:
-        channel_dict = {'channels_to_keep': {}}
+if __name__ == "__main__":
+    with open("./configs/meta_hickey.yaml", "r", encoding="utf8") as stream:
+        channel_dict = {"channels_to_keep": {}}
         metadata = yaml.safe_load(stream)
-        for channel in metadata['meta']['sample']['channels']:
-            channel_name = channel['target']
-            channel_dict['channels_to_keep'][channel_name] = process.extractOne(
-                channel_name, constants.MASTER_CHANNELS)[0]
-    with open('./configs/config_hickey.yaml', 'w') as f:
+        for channel in metadata["meta"]["sample"]["channels"]:
+            channel_name = channel["target"]
+            channel_dict["channels_to_keep"][channel_name] = process.extractOne(
+                channel_name, constants.MASTER_CHANNELS
+            )[0]
+    with open("./configs/config_hickey.yaml", "w") as f:
         yaml.dump(channel_dict, f)
